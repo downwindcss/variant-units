ed7fed987c97fc4d476df07c3b6f95c0d6df3782
dance2die <sungmkim80@gmail.com>
2021-05-20 00:07:55 -0400

    First implementation w/o tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,48 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import { PluginType } from './types';
+
+// const plugin = require('tailwindcss/plugin');
+
+/**
+ * Available CSS Units from MDN + "%", which is not in the documentation.
+ * @see https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Values_and_units#lengths
+ */
+export type Unit =
+  '%' |
+  'cm' |
+  'mm' |
+  'Q' |
+  'in' |
+  'pc' |
+  'pt' |
+  'px' |
+  'em' |
+  'ex' |
+  'ch' |
+  'rem' |
+  'lh' |
+  'vw' |
+  'vh' |
+  'vmin' |
+  'vmax';
+
+// Play: https://play.tailwindcss.com/cJmUBDX2OL
+const unitsPlugin = ({ units }: { units: Unit[] }) => ({ addVariant, e }: PluginType) => {
+  units.map((unit) => {
+    addVariant(unit, ({ container, modifySelectors }: any) => {
+      modifySelectors(({ className }: { className: string }) => `.${e(`${unit}:${className}`)}`)
+
+      container.walkRules((rule: any) => {
+        rule.walkDecls((decl: any) => {
+          if (decl.value.match(/^\d\.*\d*/g)) {
+            decl.value = decl.value.replace(/\D+$/g, unit)
+          }
+        })
+      })
+    })
+  })
+}
+
+// const unitsPlugin = plugin(emVariant({ units: ['em', 'vw', 'vh', '%'] }))
+
+
+module.exports = unitsPlugin;
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,1 @@
-import { sum } from '../src';
 
-describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
-});
